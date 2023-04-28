0f168e93eed09cb6c7f8b27a325397088d064306
Edesio Aguiar <65235867+edesioaguiar@users.noreply.github.com>
2023-04-28 19:03:19 -0300

    Atualizações 28/04.
    Atualização do MENU e espaçamento entre campos do formulário.

--- admbreno/admbreno/settings.py
+++ admbreno/admbreno/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "bootstrap4",
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
@@ -90,9 +91,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
